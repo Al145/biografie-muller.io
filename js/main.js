24c8d7bfc534db4db6def364515a104260de49fc
Al145 <47367383+Al145@users.noreply.github.com>
2019-09-12 13:56:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true
+    });
+  });
+
+// const slider = $(".owl-carousel");
+// slider.owlCarousel({
+//     items: 1,
+//     loop: true,
+//     nav: true
+// });
+
+// const owl = $(".owl-carousel");
+// owl.owlCarousel();
